8e3f14dfb8ebf3381f1c44fa3ab39ed2b5d0c92e
Kvats <kvats@alienware-ubuntu.localdomain>
2018-09-06 01:01:57 +0545

    Project added

---
+++ SecureBankingSystem/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kvats/Desktop/FoundationsToComputerSecurity/FCS-Project/SecureBankingSystem/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
